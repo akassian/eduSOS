649bee0ac3dbad1531fd1aa242548dc0bb6063c1
clarkpalmer <clark.palmer@berkeley.edu>
2019-11-21 13:48:09 -0800

    Spash page/post page connected (#1)

--- app/src/main/java/com/example/edusos/PostQuestionActivity.java
+++ app/src/main/java/com/example/edusos/PostQuestionActivity.java
@@ -2,7 +2,9 @@ package com.example.edusos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class PostQuestionActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class PostQuestionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_question);
+    }
+
+    public void onBackClick(View button) {
+        Intent myIntent = new Intent(this, MainActivity.class);
+        this.startActivity(myIntent);
     }
 }
