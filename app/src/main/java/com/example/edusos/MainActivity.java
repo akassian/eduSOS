8f7d7eab0cc18bd8d5333ae6d7a03e06efd7ca1f
Andrei Kassiantchouk <akass1122@gmail.com>
2019-11-23 00:08:39 -0800

    Good layout, sign in works.

--- app/src/main/java/com/example/edusos/MainActivity.java
+++ app/src/main/java/com/example/edusos/MainActivity.java
@@ -13,6 +13,10 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -21,6 +25,14 @@ import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+
+
+
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,6 +42,11 @@ public class MainActivity extends AppCompatActivity {
 
     ArrayList<Question> allQuestions;
     DatabaseReference dbQuestion;
+
+    // For Sign in
+    GoogleSignInClient mGoogleSignInClient;
+    private static final int RC_SIGN_IN = 007;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +62,7 @@ public class MainActivity extends AppCompatActivity {
                 searchQuestion(searchText);
             }
         });
+
 
         postButton = findViewById(R.id.postButton);
         postButton.setOnClickListener(new View.OnClickListener() {
@@ -115,4 +133,69 @@ public class MainActivity extends AppCompatActivity {
 
         startActivity(intent);
     }
+
+    // For Sign in
+    public void onLoginClick(View button) {
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        updateUI(account);
+        signIn();
+    }
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        //startActivityForResult(signInIntent,1);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            Log.d("SIGNIN_!: ", account.getDisplayName() + ",   " + account.getEmail());
+
+            //String personName = account.getDisplayName();
+
+//            if (account.getPhotoUrl()!= null) {
+//                String personPhotoUrl = account.getPhotoUrl().toString();
+//            }
+
+            //String email = account.getEmail();
+
+            //txt_welcome.setText("Welcome "+personName+"\n"+ email);
+
+            updateUI(account);   // Signed in successfully, show authenticated UI.
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w("SIGNIN_", "signInResult:failed code=" + e.getStatusCode());
+            Log.d("SIGNIN_", "signInResult:failed code=" + e.getStatusCode());
+            updateUI(null);
+        }
+    }
+
+    private void updateUI(GoogleSignInAccount account) {
+        // For example, display Welcome name if account is not null
+        if (account != null) {
+            //Toast.makeText(this, account.getDisplayName(), Toast. LENGTH_LONG).show();
+            Log.d("SIGNIN_UI", account.getDisplayName() + ",   " + account.getEmail());
+        }
+
+
+    }
+
 }
